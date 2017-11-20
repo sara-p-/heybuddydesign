ffa90549c3ef46dce39499298c1b6b4d3d9c02da
Sara Pitt <sarapitt88@gmail.com>
2017-11-20 13:40:41 +0800

    init repo

---
+++ Local Sites/spcreativeus/app/sql/local.wp_usermeta.sql
@@ -0,0 +1,20 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_usermeta` VALUES
+(1,1,"nickname","spcreative-admin"),
+(2,1,"first_name",""),
+(3,1,"last_name",""),
+(4,1,"description",""),
+(5,1,"rich_editing","true"),
+(6,1,"comment_shortcuts","false"),
+(7,1,"admin_color","fresh"),
+(8,1,"use_ssl","0"),
+(9,1,"show_admin_bar_front","true"),
+(10,1,"locale",""),
+(11,1,"wp_capabilities","a:1:{s:13:\"administrator\";b:1;}"),
+(12,1,"wp_user_level","10"),
+(13,1,"dismissed_wp_pointers",""),
+(14,1,"show_welcome_panel","1"),
+(15,1,"session_tokens","a:1:{s:64:\"f9494397eba27463d8f8ba1b0dc8a48c49c79ccdf73f75b0bcb175f2f05eb150\";a:4:{s:10:\"expiration\";i:1486659324;s:2:\"ip\";s:10:\"172.17.0.1\";s:2:\"ua\";s:120:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36\";s:5:\"login\";i:1486486524;}}"),
+(16,1,"wp_dashboard_quick_press_last_post_id","3");
---
+++ Local Sites/spcreativeus/app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"spcreative-admin","$P$BD.dmsPsUQ1MkiY8l0eUkRmwBKKEkf/","spcreative-admin","sara@sp-creative.us","","2017-02-07 16:54:43","",0,"spcreative-admin");
